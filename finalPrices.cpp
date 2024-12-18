5f4f58c5cb4cdae7be8cafe3e3c6814a0120a31c
TanishaMehta17 <121849870+TanishaMehta17@users.noreply.github.com>
2024-12-18 09:07:32 +0530

    finalPrices

---
+++ finalPrices.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+    stack<int> s;
+    vector<int> ans(prices.size());
+
+    for (int i = prices.size() - 1; i >= 0; i--) {
+        while (!s.empty() && s.top() > prices[i]) {
+            s.pop();
+        }
+        if (s.empty()) {
+            ans[i] = prices[i];
+        } else {
+            ans[i] = prices[i]- s.top();
+        }
+        s.push(prices[i]);
+    }
+    return ans;
+    }
+};
